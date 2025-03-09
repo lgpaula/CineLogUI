da77a063144ac77dfdf61fc047c16a946afd2314
lgpaula <l.gdepaula@hotmail.com>
2025-03-09 16:20:56 +0100

    simple pane and content

--- CineLog/ViewModels/MainWindowViewModel.cs
+++ CineLog/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,30 @@
+// handle logic, button clicks and binds data to the UI
+using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
 using ReactiveUI;
-using System.Windows.Input;
 
-namespace CineLog.ViewModels
+namespace CineLog.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
 {
-    public class MainWindowViewModel : ViewModelBase
+    // ObservableCollection is like a List, but updates the UI when changed
+    public ObservableCollection<string> Buttons { get; } = new()
     {
-        public ICommand BuyMusicCommand { get; }
+        "Button 1", "Button 2", "Button 3", "Button 4", "Button 5"
+    };
 
-        public MainWindowViewModel()
-        {
-            BuyMusicCommand = ReactiveCommand.Create(() =>
-            {
-                // Code here will be executed when the button is clicked.
-            });
-        }
+    // Reactive Command for handling button clicks
+    public ReactiveCommand<string, Unit> ButtonClickCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        // Define what happens when a button is clicked
+        ButtonClickCommand = ReactiveCommand.Create<string>(OnButtonClick);
+    }
+
+    private void OnButtonClick(string buttonText)
+    {
+        Console.WriteLine($"Button clicked: {buttonText}");
     }
 }
--- CineLog/Views/MainWindow.axaml.cs
+++ CineLog/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using CineLog.ViewModels;
 
 namespace CineLog.Views;
 
