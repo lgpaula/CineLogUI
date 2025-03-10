0c681f3870c3f89f3895420f9151c725e74a0e3f
lgpaula <l.gdepaula@hotmail.com>
2025-03-10 13:12:54 +0100

    working button presses

--- CineLog/ViewModels/MainWindowViewModel.cs
+++ CineLog/ViewModels/MainWindowViewModel.cs
@@ -8,23 +8,15 @@ namespace CineLog.ViewModels;
 
 public class MainWindowViewModel : ReactiveObject
 {
+
     // ObservableCollection is like a List, but updates the UI when changed
     public ObservableCollection<string> Buttons { get; } = new()
     {
         "Button 1", "Button 2", "Button 3", "Button 4", "Button 5"
     };
 
-    // Reactive Command for handling button clicks
-    public ReactiveCommand<string, Unit> ButtonClickCommand { get; }
-
-    public MainWindowViewModel()
+    public void HandleButtonClick()
     {
-        // Define what happens when a button is clicked
-        ButtonClickCommand = ReactiveCommand.Create<string>(OnButtonClick);
-    }
-
-    private void OnButtonClick(string buttonText)
-    {
-        Console.WriteLine($"Button clicked: {buttonText}");
+        Console.WriteLine("Button clicked from ViewModel");
     }
 }
--- CineLog/Views/MainWindow.axaml.cs
+++ CineLog/Views/MainWindow.axaml.cs
@@ -1,12 +1,28 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 using CineLog.ViewModels;
 
 namespace CineLog.Views;
 
 public partial class MainWindow : Window
 {
+    private MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext!;
+
     public MainWindow()
     {
         InitializeComponent();
     }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private void ClickHandler(object sender, RoutedEventArgs e)
+    {
+        // Call a method in the ViewModel
+        ViewModel.HandleButtonClick();
+    }
+
 }
