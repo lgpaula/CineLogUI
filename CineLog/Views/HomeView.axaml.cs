6762ca64fc2ab5190be15b038458fed57514b68c
lgpaula <l.gdepaula@hotmail.com>
2025-03-12 19:17:11 +0100

    Fixed button placements

--- CineLog/Views/HomeView.axaml.cs
+++ CineLog/Views/HomeView.axaml.cs
@@ -1,12 +1,15 @@
 using System;
 using Avalonia.Controls;
-using Avalonia.Threading;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using CineLog.ViewModels;
 
 namespace CineLog.Views
 {
     public partial class HomeView : UserControl
     {
+        private MainWindowViewModel ViewModel => (MainWindowViewModel)DataContext!;
+
         public HomeView()
         {
             InitializeComponent();
@@ -17,6 +20,14 @@ namespace CineLog.Views
             AvaloniaXamlLoader.Load(this);
             // You can do additional initialization here, directly on the UI thread
             Console.WriteLine("home");
+        }
+
+        private void ClickHandler(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is string viewName)
+            {
+                ViewModel.HandleButtonClick(viewName);
+            }
         }
     }
 }
